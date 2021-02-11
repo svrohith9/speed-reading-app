a46496bdd013e79475d7d8f0328039d4fde8d741
svrohith9 <svrohith999@outlook.com>
2021-02-11 19:30:07 +0530

    ignore defaults

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello</p>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
